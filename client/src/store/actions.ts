cae5e04aae6e36189acf2944511945375ae09c7b
Eray <129968185+deverays@users.noreply.github.com>
2024-05-11 16:58:58 +0300

    Update actions.ts

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -1,13 +1,9 @@
-import { getReq, postReq } from "../utils/axiosReqs";
+import { getReq } from "../utils/axiosReqs";
 import { useRouter } from "vue-router";
 
-type UserData = {
+interface UserData {
     access_token?: string;
-};
-
-const ActionTypes = {
-    SAVE_GUILD_STATE: "saveGuildState",
-};
+}
 
 export default {
     async initUser() {
@@ -20,7 +16,7 @@ export default {
             return;
         }
 
-        (this as any)._isLoading = true;
+        (this as any).isLoading = true;
 
         const getUser = async () => {
             return await getReq("/user");
@@ -29,44 +25,11 @@ export default {
         try {
             const response = await this.makeRequest(getUser);
             if (response.data.success) {
-                (this as any).getters._getUser = response.data;
                 (this as any)._isLogin = true;
+                (this as any).getters._getUser = response.data.user_data;
             }
         } finally {
             (this as any)._isLoading = false;
-        }
-    },
-
-    async initGuild() {
-        const userData: UserData = JSON.parse(
-            localStorage.getItem("user_data") ?? "{}"
-        );
-
-        if (!userData.access_token) {
-            return false;
-        }
-
-        const getGuilds = async () => {
-            return await getReq("/guilds");
-        };
-
-        const response = await this.makeRequest(getGuilds);
-        if (response.data.success) {
-            (this as any).getters._getGuilds = response.data;
-        }
-
-        return response;
-    },
-
-    async dispatch(
-        actionType: keyof typeof ActionTypes,
-        { guild_id, data }: { guild_id?: string; data: object }
-    ) {
-        switch (actionType) {
-            case ActionTypes.SAVE_GUILD_STATE:
-                return await postReq(`/guilds/${guild_id}`, data);
-            default:
-                throw new Error(`Unknown action type: ${actionType as string}`);
         }
     },
 
@@ -77,14 +40,18 @@ export default {
         } catch (err: any) {
             await this.handleRateLimit(err, () => this.makeRequest(requestFunc));
             if (err?.response?.status === 401) {
-                console.log(err.response.data);
+                console.log(err.response.data)
                 this.handleUnauthorized();
             }
             throw err;
         }
     },
 
-    handleRateLimit(err: any, retryCallback: () => Promise<any>) {
+    async handleRateLimit(
+        this: any,
+        err: any,
+        retryCallback: () => Promise<any>
+    ) {
         if (err?.response?.data?.message === "You are being rate limited.") {
             const retryAfter = err.response.data.retry_after || 1;
             setTimeout(retryCallback, retryAfter * 1300);
