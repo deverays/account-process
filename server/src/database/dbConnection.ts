9fa5a638f440e52a8b2d85f54e6bcdc1d07e95d8
Eray <129968185+deverays@users.noreply.github.com>
2024-05-12 22:17:57 +0300

    Delete server directory

--- server/src/database/dbConnection.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import * as config from "../../config.json";
-
-export default async () =>
-    await mongoose
-        .connect(config.database.url)
-        .catch((err) => console.log(err));
