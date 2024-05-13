f4904d89c98acb811ca1df4f929b0caf640fcc31
Eray <129968185+deverays@users.noreply.github.com>
2024-05-14 01:14:37 +0300

    Update i18n.ts

--- client/src/plugins/i18n.ts
+++ client/src/plugins/i18n.ts
@@ -1,7 +1,6 @@
 import { App } from "vue";
 import i18next, { InitOptions } from "i18next";
 import I18NextVue from "i18next-vue";
-
 import tr from "../../public/locales/tr.json";
 import enUs from "../../public/locales/en-US.json";
 
@@ -10,56 +9,60 @@ export interface Language {
     lng: string;
 }
 
-const defaultLanguage: string = "en-US";
-const localStorageKey: string = "lng";
+const BASE_LANGUAGE = "en-US";
+const BASE_LOCAL_STORAGE_KEY = "lang";
 
-const languages: Language[] = [
+export const languages: Language[] = [
     { name: "Türkçe", lng: "tr" },
     { name: "English", lng: "en-US" },
 ];
 
-const languageFromLocalStorage: string | null =
-    localStorage.getItem(localStorageKey);
-const userLanguage: string = window.navigator.language || defaultLanguage;
-const selectedLanguage: Language =
-    languages.find(
-        (lang) => lang.lng === (languageFromLocalStorage || userLanguage)
-    ) || languages.find((lang) => lang.lng === defaultLanguage)!;
+function getInitialLanguage(): Language {
+    const languageFromLocalStorage = localStorage.getItem(BASE_LOCAL_STORAGE_KEY);
+    const userLanguage = window.navigator.language || BASE_LANGUAGE;
+    return (
+        languages.find(
+            (lang) => lang.lng === (languageFromLocalStorage || userLanguage)
+        ) || languages.find((lang) => lang.lng === BASE_LANGUAGE)!
+    );
+}
 
-const i18nOptions: InitOptions = {
-    lng: selectedLanguage.lng,
-    fallbackLng: "en-US",
-    fallbackNS: "common",
-    resources: {
-        "en-US": { translation: enUs },
-        tr: { translation: tr },
+function changeLanguage(lang: string) {
+    i18next.changeLanguage(lang);
+    localStorage.setItem(BASE_LOCAL_STORAGE_KEY, lang);
+    document.documentElement.lang = lang;
+}
+
+export default {
+    languages,
+    install(app: App) {
+        const initialLanguage = getInitialLanguage();
+
+        if (
+            !languages.some(
+                (lang) => lang.lng === localStorage.getItem(BASE_LOCAL_STORAGE_KEY)
+            )
+        ) {
+            localStorage.setItem(BASE_LOCAL_STORAGE_KEY, initialLanguage.lng);
+        }
+
+        app.config.globalProperties.$i18n = {
+            language: initialLanguage,
+            languages: languages,
+            changeLanguage,
+        };
+
+        i18next.init<InitOptions>({
+            lng: initialLanguage.lng,
+            fallbackLng: "en-US",
+            resources: {
+                "en-US": { translation: enUs },
+                tr: { translation: tr },
+            },
+        });
+
+        document.documentElement.lang = initialLanguage.lng;
+
+        app.use(I18NextVue, { i18next });
     },
 };
-
-const changeLanguage = (lang: string): void => {
-    i18next.changeLanguage(lang);
-    localStorage.setItem(localStorageKey, lang);
-};
-
-const installI18nPlugin = (app: App): void => {
-    if (!languages.some((lang) => lang.lng === languageFromLocalStorage))
-        localStorage.setItem(localStorageKey, selectedLanguage.lng);
-
-    app.config.globalProperties.$i18n = {
-        languages,
-        changeLanguage,
-    };
-
-    i18next.init(i18nOptions);
-
-    app.use(I18NextVue, { i18next });
-};
-
-const I18nPlugin = {
-    languages,
-    selectedLanguage,
-    changeLanguage,
-    install: installI18nPlugin,
-};
-
-export default I18nPlugin;
